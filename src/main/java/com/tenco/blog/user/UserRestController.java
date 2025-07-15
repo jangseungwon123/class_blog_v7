88a9e08146fabed7539bdc6448c8ca6c5119b3fd
jang seungwon <rhanwnf97@naver.com>
2025-07-15 10:44:56 +0900

    USER 도메인 REST API 수정 처리

--- src/main/java/com/tenco/blog/user/UserRestController.java
+++ src/main/java/com/tenco/blog/user/UserRestController.java
@@ -1,16 +1,14 @@
 package com.tenco.blog.user;
 
 import com.tenco.blog._core.common.ApiUtil;
+import com.tenco.blog.utils.Define;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -44,9 +42,52 @@ public class UserRestController {
         return ResponseEntity.status(HttpStatus.CREATED)
                     .body(new ApiUtil<>(joinUser));
     }
-    // 로그인 요청
-    // 회원 정보 조회
+
+
+    // 로그인 API
+    // http://localhost:8080/login
+    @PostMapping("/login")
+    public ResponseEntity<ApiUtil<UserResponse.LoginDTO>> login(
+            @RequestBody UserRequest.LoginDTO reqDTO, HttpSession session) {
+        log.info("로그인 API 호출 - 사용자명 {}", reqDTO.getUsername());
+        reqDTO.validate();
+        UserResponse.LoginDTO loginUser = userService.login(reqDTO);
+        session.setAttribute(Define.SESSION_USER,loginUser);
+        return ResponseEntity.ok(new ApiUtil<>(loginUser));
+    }
+
+
+    // 회원 정보 API
+    @GetMapping("/api/users/{id}")
+    public ResponseEntity<ApiUtil<UserResponse.DetailDTO>> getUserInfo(
+            @PathVariable(name = "id") Long id, HttpSession session){
+
+        log.info("회원정보 API 호출 - ID :{}",id);
+
+        User sessionUser = (User) session.getAttribute(Define.SESSION_USER);
+        UserResponse.DetailDTO userDetail = userService.findUserById(id,sessionUser);
+        return ResponseEntity.ok(new ApiUtil<>(userDetail));
+
+    }
+
+
     // 회원 정보 수정
+    @PutMapping("/api/users/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable(name = "id")Long id,
+                                        @RequestBody UserRequest.UpdateDTO updateDTO) {
+       // 인증검사는 인터셉터에 처리 됨
+        // 유효성 검사
+        updateDTO.validate();
+        UserResponse.UpdateDTO updateUser = userService.updateById(id, updateDTO);
+        return ResponseEntity.ok(new ApiUtil<>(updateUser));
+    }
+
     // 로그아웃 처리
+    @GetMapping("/logout")
+    public ResponseEntity<ApiUtil<String>> logout(HttpSession session){
+        log.info("로그아웃 API 호출");
+        session.invalidate(); // 세션 무효화 메서드
+        return ResponseEntity.ok(new ApiUtil<>("로그아웃 완료"));
+    }
 
 }
